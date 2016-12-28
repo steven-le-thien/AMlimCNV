ffc6937d34397a50df7df08a968dfb44a02fab77
thienle <steven.le.thien@gmail.com>
2016-12-27 21:55:19 -0500

    refactoring

--- R/main.R
+++ R/main.R
@@ -19,7 +19,7 @@ test_original_coverage <- read_data(test.data.dir, "Original.Coverage")
 
 #Sampling
 print("Investigating population...")
-normal_sampling_report <- lapply(1:(dim(reference_coverage)[2]-2), single_test, reference_coverage, test, ref_quality, test_quality)
+normal_sampling_report <- lapply(1:(dim(reference_coverage)[2]-2), single_test, reference_coverage, test, reference_quality, test_quality)
 print("...done")
 
 #Sampling analysis (inferential)
@@ -45,7 +45,7 @@ pvalue2sided <- 2*pnorm(-abs(z_score))
 
 #Begin test series
 print("Testing disomic hypothesis")
-lapply(1:length(pvalue2sided), report_cnv, p_value = pvalue2sided, z_score, test_report, pop_mean, pop_sd, names(z_score))
+result <- lapply(1:length(pvalue2sided), report_cnv, p_value = pvalue2sided, z_score, test_report, pop_mean, pop_sd, names(z_score))
 
 print("...done")
 
